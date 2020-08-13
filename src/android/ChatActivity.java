fe443438175eeeb4b0c4d2d8a5c5187583f77755
nvhai142 <61268096+nvhai142@users.noreply.github.com>
2020-08-13 17:16:57 +0700

    Update ChatActivity.java

--- src/android/ChatActivity.java
+++ src/android/ChatActivity.java
@@ -41,7 +41,7 @@ public class ChatActivity extends AppCompatActivity implements SwipeBackLayout.S
     private static final String TAG = ChatActivity.class.getSimpleName();
     private static final String LIVEPERSON_FRAGMENT = "liveperson_fragment";
     private ConversationFragment mConversationFragment;
-    String BrandID = "63168344";
+    String BrandID = "47817293";
     String AppID = "com.exchange.demoliveperson";
 
     private Menu mMenu;
