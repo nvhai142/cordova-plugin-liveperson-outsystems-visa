78bf43e3f17bd6627f8f6ea696f2fee73e066791
nvhai142 <61268096+nvhai142@users.noreply.github.com>
2020-11-06 15:02:42 +0700

    Update ConversationVC.swift

--- src/ios/ConversationVC.swift
+++ src/ios/ConversationVC.swift
@@ -47,7 +47,7 @@ class ConversationVC: UIViewController, LPMessagingSDKdelegate {
         //}
     }
     var conversationQuery:ConversationParamProtocol?;
-    var alert = UIAlertController(title: nil, message: "Loading...", preferredStyle: .alert)
+    var alert = UIAlertController(title: nil, message: "", preferredStyle: .alert)
 
     override func viewDidLoad() {
         super.viewDidLoad()
